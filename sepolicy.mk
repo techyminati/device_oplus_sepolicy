607c55de6a4c17b70604985be5daf120f4a517f0
TheMalachite <eliasgheeraert@gmail.com>
2021-04-19 19:44:16 +0200

    sepolicy: Initial qualcomm plat private sepolicy rules
    Signed-off-by: TheMalachite <eliasgheeraert@gmail.com>

--- sepolicy.mk
+++ sepolicy.mk
@@ -1,8 +1,15 @@
 # Oplus specific SELinux policy variable definitions
 SEPOLICY_OPLUS_PATH := device/oplus/sepolicy
 
+# Generic sepolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     $(SEPOLICY_OPLUS_PATH)/common/private
 
 BOARD_VENDOR_SEPOLICY_DIR += \
     $(SEPOLICY_OPLUS_PATH)/common/vendor
+
+# SoC specific sepolicy
+ifeq ($(BOARD_USES_QCOM_HARDWARE), true)
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    $(SEPOLICY_OPLUS_PATH)/qualcomm/private
+endif
